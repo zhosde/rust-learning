42a906d5f5b722ed08ee32f7fe661d13d2faa584
zhosde <zhou.hongshan636@gmail.com>
2022-12-09 13:53:47 +0100

    with result

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,13 +2,13 @@ const MAX_POINTS: u32 = 100_000; // constant
 
 fn main() {
     let mut x = 5;
-    println!("The value of x is {}", x);
+    println!("The value of x is {}", x); // 5
     x = 6;
-    println!("The value of x is {}", x);
+    println!("The value of x is {}", x); // 6
 
     let y = 2;
     let y = y + 1; // shadowing
-    println!("The value of x is {}", y);
+    println!("The value of y is {}", y); // 3
 
     let spaces = "   ";
     let spaces = spaces.len(); // shadowing, type changable
@@ -20,9 +20,9 @@ fn main() {
 
     let tup: (i32, f64, u8) = (500, 6.4, 1); // compound type, tuple
     let (a, b, c) = tup;
-    println!("{}, {}, {}", a, b, c );
+    println!("{}, {}, {}", a, b, c ); // 500, 6.4, 1
     let one = tup.2;
-    println!("{}", one);
+    println!("{}", one); // 1
 
     let _a: [i32; 5] = [1,2,3,4,5];
     let _b = [3; 5]; // [3,3,3,3,3]
@@ -31,11 +31,11 @@ fn main() {
     example();
 
     let f = plus_five(5);
-    println!("The value of f is {f}");
+    println!("The value of f is {f}"); // 10
 
     let condition = true;
     let number = if condition {5} else {6}; // arms shall have compatible type, e.g. not "6"
-    println!("The number value is {number}");
+    println!("The number value is {number}"); // 5
 
     let mut counter = 0;
     let result = loop {
@@ -44,7 +44,7 @@ fn main() {
             break counter * 2;
         }
     };
-    println!("The result if {result}");
+    println!("The result if {result}"); // 20
 
     let arr = [10, 20, 30];
     for ele in arr {
@@ -64,7 +64,7 @@ fn example(){
         let p = 1;
         p + 2 // no semicolon, expression has returned value; p+2; => statement will return ()
     };
-    println!("The value of n is {n}")
+    println!("The value of n is {n}") // 3
 }
 
 fn plus_five(x: i32) -> i32 {
