60e977af8071a7a2e54f4a60dee372bcb2c74abd
zhosde <zhou.hongshan636@gmail.com>
2022-12-09 15:03:39 +0100

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,91 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32); // tuple struct
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    length: u32,
+}
+
+impl Rectangle { // method
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+
+    fn square(size: u32) -> Rectangle { // doesn't have self as 1st parameter => associated function
+        Rectangle {
+            width: size,
+            length: size,
+        }
+    }
+}
+
+fn _build_user(email: String, username: String) -> User {
+    User {
+        email, // field init shorthand syntax
+        username, 
+        active: true,
+        sign_in_count: 0,
+    }
+}
+
+fn main () {
+    let user1 = User {
+        email: String::from("abc@email.com"),
+        username: String::from("Alice"),
+        active: true,
+        sign_in_count: 555,
+    };
+
+    let _user2 = User {
+        email: String::from("efg@email.com"),
+        username: String::from("Bett"),
+        ..user1 // struct update syntax
+    };
+
+    let _black = Color(0,0,0);
+
+    let rect = Rectangle {
+        width: 30,
+        length: 50,
+    };
+
+    //println!("{}", area(&rect));
+    println!("{}", rect.area());
+    println!("{:#?}", rect);
+
+    let rect1 = Rectangle {
+        width: 30, 
+        length: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 10, 
+        length: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 35, 
+        length: 55,
+    };
+
+    println!("{}", rect1.can_hold(&rect2));
+    println!("{}", rect1.can_hold(&rect3));
+
+    let _s = Rectangle::square(20); // calling associated function
+}
+/* 
+fn area(rect: &Rectangle) -> u32 {
+    rect.width * rect.length
+}
+*/
